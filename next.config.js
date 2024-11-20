2d2d38c976fbbd6e9d58b459e38f20cafb2a5bbc
Jeff MacDonald <macjeffrey@gmail.com>
2024-11-19 23:01:16 -0500

    Trying to fix Vercel deployment

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['your-domain.com'], // Add domains for external images
+  },
+}
+
+module.exports = nextConfig 
